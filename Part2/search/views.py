3f95062942439ea85df8363a7cb64ad281c9b0ec
Dilnawaz Warisi <dilnawazwarisi@91springboard.com>
2020-04-26 22:10:50 +0530

    Added code to reuse the query index feature built in part 1 and author api to complete the search application

--- Part2/search/views.py
+++ Part2/search/views.py
@@ -1,6 +1,26 @@
 from rest_framework.views import APIView
+from rest_framework import status
+from django.http import JsonResponse
+
+
+#Local source tree imports
+from .author_service import _author
+from Part1.query_index import _query_service
 
 
 class SearchView(APIView):
     def post(self, request):
-        pass
+        data = request.data
+        result=[]
+        num_of_results = data['K']
+        for query in data['queries']:
+            query_obj = _query_service.query_index(query,num_of_results) # Get the matching summaries
+            if query_obj:
+                for item in query_obj:
+                    response = _author.api_call(method='POST',
+                                                data='{"book_id":' + str(item['id']) + '}'
+                                                )
+                    item['query']=query
+                    item['author'] = response.json()['author']
+                result.append(query_obj)
+        return JsonResponse(result,status=status.HTTP_200_OK, safe=False)
